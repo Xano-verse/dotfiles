c023d80430ded748b11a0925b0a3e5eb7ee5d4f7
Xano-verse <alexcdelg@gmail.com>
2024-06-20 23:18:01 +0100

    background battery level fica vermelho quando abaixo de 10%

--- dwm/dotdwm/autostart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-feh --bg-scale ~/Pictures/Wall\ of\ papers/Horizontal/Not\ Themed/minimalist\ town.png
-setxkbmap -layout pt
-picom &
-export PATH=/home/xanodeb/dwm/dwmblocks/statusbar_scripts:$PATH
-dwmblocks &
